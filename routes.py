94d045bc28b5334f8cbadb3d8e2bf742e51fea0c
natthas <32890999+natthas@users.noreply.github.com>
2020-12-18 11:19:03 +0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,13 +1,17 @@
 __author__ = 'kai'
-
+#registerpage2.html
 from flask import Flask, render_template, request
 
 app = Flask(__face__)
 
 @app.route('/reg', methods=['POST'])
 def reg():
-    image = request.form['image']
-    return image
+    img1 = request.form['img1']
+    img2 = request.form['img2']
+    img3 = request.form['img3']
+    img4 = request.form['img4']
+    img5 = request.form['img5']
+    return [img1,img2,img3,img4,img5]
 
 if __face__ == '__main__':
     app.run(host = '0.0.0.0', port = 3000)
